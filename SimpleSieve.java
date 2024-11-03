fbeec57b99e5640b8fa608758e4a2069d3f85255
Tharun-0221 <mynameistharun02@gmail.com>
2024-11-03 18:17:22 +0530

    SimpleSieve.java

--- SimpleSieve.java
+++ SimpleSieve.java
@@ -1,1 +1,29 @@
+import java.util.Arrays;
+public class SimpleSieve{
+    public static void main(String[] args) {
+        int n = 100; // Find all primes up to n
+        boolean[] isPrime = new boolean[n + 1];
+
+        // Initialize all numbers as prime
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false; // 0 and 1 are not prime
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                // Mark multiples of i as false
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        // Print all prime numbers
+        System.out.println("Prime numbers up to " + n + ":");
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
 
